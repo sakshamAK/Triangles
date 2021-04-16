2d0851f259cd3c4dc9ff98caefcac173b2fcf180
nikenugium <saksham.8484@gmail.com>
2021-04-16 19:41:39 +0530

    feat: links added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,10 +2,10 @@ const Header = () => {
     return (
         <header>
             <ul>
-            <div>Created By <a href = "https://www.sakshamak.netlify.app">Saksham</a></div>
-            <li><img alt = "twitter" src = "./twitter.svg" /></li>
-            <li><img alt = "linked" src = "./linkedIn.svg" /></li>
-            <li><img alt = "github" src = "./github.svg" /></li>
+            <div>Created By <a href = "https://sakshamak.netlify.app/">Saksham</a></div>
+            <li><a href = "https://twitter.com/Saksham_ka"><img alt = "twitter" src = "./twitter.svg" /></a></li>
+            <li><a href = "https://www.linkedin.com/in/saksham-ak-55b9131b3/"><img alt = "linked" src = "./linkedIn.svg" /></a></li>
+            <li><a href = "https://github.com/sakshamAK"><img alt = "github" src = "./github.svg" /></a></li>
             </ul>
         </header>
     )
